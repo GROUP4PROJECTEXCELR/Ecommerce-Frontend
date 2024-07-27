df98b13b9cf0dea002d3abcc5f9f1ffe31f87bcb
GROUP4PROJECTEXCELR <groupprojectexcelr@gmail.com>
2024-07-28 02:35:06 +0530

    Update Item.jsx
    Signed-off-by: GROUP4PROJECTEXCELR <groupprojectexcelr@gmail.com>

--- src/components/Item/Item.jsx
+++ src/components/Item/Item.jsx
@@ -2,10 +2,8 @@ import React from 'react'
 import './Item.css'
 
 const  Item = (props) => {
-<<<<<<< HEAD
-  
-=======
->>>>>>> 72af270e7e7491f6b368eab073373b4bf4ffc4e9
+
+
   return (
     <div className='item'>
     <img src={props.image} alt='' className='image'></img>
