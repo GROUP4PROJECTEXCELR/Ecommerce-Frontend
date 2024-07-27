bdca55fb410d53a7c54c84c62121f8d9b5b3c400
GROUP4PROJECTEXCELR <groupprojectexcelr@gmail.com>
2024-07-28 02:33:54 +0530

    Update App.js
    Signed-off-by: GROUP4PROJECTEXCELR <groupprojectexcelr@gmail.com>

--- src/App.js
+++ src/App.js
@@ -8,11 +8,9 @@ import { Product } from './webpages/Product';
 import { Cart } from './webpages/Cart';
 import { Login } from './webpages/Login';
 import {Footer} from './components/Footer/Footer'
-<<<<<<< HEAD
 
-=======
-//shdfosfoisdjfooo
->>>>>>> 72af270e7e7491f6b368eab073373b4bf4ffc4e9
+
+
 function App() {
   return (
     <div>
@@ -22,15 +20,13 @@ function App() {
         <Hero/>
         <Routes>
           <Route path="/" element={<Shop />} />
-<<<<<<< HEAD
+
           <Route path="mens" element={<CategoryItem category="Men" />} />
           <Route path="womens" element={<CategoryItem category="Women" />} />
           <Route path="kids" element={<CategoryItem category="Kids" />} />
-=======
-          <Route path="mens" element={<CategoryItem items="men" />} />
-          <Route path="womens" element={<CategoryItem items="women" />} />
-          <Route path="kids" element={<CategoryItem items="kid" />} />
->>>>>>> 72af270e7e7491f6b368eab073373b4bf4ffc4e9
+
+          
+
           <Route path="product" element={<Product />} />
             
             <Route path=":productId" element={<Product />} />
